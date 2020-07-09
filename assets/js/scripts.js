607063bb3a97a71834442de7b414abf6f96ca885
Naim Sedán <naim.alejandro.sedan@gmail.com>
2020-07-09 01:34:57 +0100

    Delete tasks button function added.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,16 @@
-$('.fa-plus').click(function () {
+
+$(document).on('click', '.fa-plus', function(){
     if ($('.form-control').val()) {
         let newTask = $('.form-control').val();
-        let taskBlock = '<div class="task"><div class="input-group"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox"></div></div><div class="task-text"><span>' + newTask + '</span></div><div class="input-group-append"><span class="input-group-text"><i class="fa fa-edit"></i></span><span class="input-group-text"><i class="fa fa-trash"></i></span></div></div></div>'
+        let taskBlock = '<div class="task"><div class="input-group"><div class="input-group-prepend"><div class="input-group-text"><input class="checkbox" type="checkbox"></div></div><div class="task-text">'+newTask+'</div><div class="input-group-append"><span class="input-group-text"><i class="fa fa-edit"></i></span><span class="input-group-text"><i class="fa fa-trash"></i></span></div></div></div>'
         $('.task-list').append(taskBlock)
         $('.form-control').val("")
     } else {
-        alert('Empty')
+        alert("'Nothing' is not a task!")
     }
 })
 
 
-
-$('.fa-trash').click(function () {
-    $(".task").remove();
-})
+$(document).on('click', '.fa-trash', function(){
+    $(this).closest('.task').remove();
+});
